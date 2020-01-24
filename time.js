8893d02c2b0a8ead929d46f38e66f66ad3ce39f0
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-01-24 14:54:27 -0800

    formatSeconds

--- time.js
+++ time.js
@@ -1,18 +1,59 @@
-function formatSecond(seconds){
+function formatSecond(totalSeconds){
    let answer;
-   let secs;
+   let seconds;
+   let totalMinutes;
    let minutes;
+   let totalHours;
    let hours;
+   let totalDays;
    let days;
-    if (seconds <=59){
-        return [seconds + 's'];
+   let totalWeeks;
+    if (totalSeconds <=59){
+        return [totalSeconds + 's'];
     }
-    if (seconds >=60){
-        minutes= Math.floor(seconds/60)
-        hours= Math.floor(minutes/60)
-        days=Math.floor(hours/24);
-        secs= seconds - minutes*60;
+    if (totalSeconds >=60){
+        totalMinutes= Math.floor(totalSeconds/60)
+
+        totalHours= Math.floor(totalMinutes/60)
+
+        totalDays= Math.floor(totalHours/24);
+
+        totalWeeks= Math.floor(totalDays/7);
+
+        seconds= totalSeconds - totalMinutes*60;
+
+        minutes =totalMinutes - totalHours*60
+
+        hours = totalHours- totalDays*24
+
+        days= totalDays - totalWeeks*7;
     }
-    return answer= [days + 'd' + hours+ 'h' + minutes + 'm' + secs + 's'];
-}
-console.log(formatSecond(100));
+
+    if(totalWeeks===0 && days !==0){
+      answer=  days + 'd ' + hours+ 'h ' + minutes + 'm ' + seconds + 's';
+      return answer;
+    }
+
+    if(days ===0 && totalWeeks===0 && hours!==0){
+        answer= hours+ 'h ' + minutes + 'm ' + seconds + 's';
+        return answer;
+    }
+
+    if(hours===0 && days ===0 && totalWeeks===0 && minutes!==0){
+         answer= minutes + 'm ' + seconds + 's';
+         return answer;
+    }
+
+    if(minutes===0 &&hours===0 && days ===0 && totalWeeks===0){
+        answer= seconds + 's';
+        return answer;
+    }
+
+    return answer =  totalWeeks + 'w ' + days + 'd ' + hours+ 'h ' + minutes + 'm ' + seconds + 's ';    }
+
+
+console.log(formatSecond(120));
+console.log(formatSecond(604800));
+console.log(formatSecond(86400));
+console.log(formatSecond(3645));
+console.log(formatSecond(89460));
